65b55850d80da256b2218500064b0bbf6881be91
Sakshi2020037 <iit2020037@iiita.ac.in>
2024-06-13 10:45:16 +0530

    send message bar design created- msg type added and move to bottom

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./chat.css";
+import { useState } from "react";
 
 const Chat = () => {
+  const [text, setText] = useState("");
+  console.log(text);
+
   return (
     <div className="chat">
       <div className="top">
@@ -25,7 +29,12 @@ const Chat = () => {
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" placeholder="Type a message..." />
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <div className="emoji">
           <img src="./emoji.png" alt="" />
         </div>
